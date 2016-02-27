028a5978478e75e154cd8f70e3af5a942adcb505
a-regal <andresregal97@gmail.com>
2016-02-26 19:07:11 -0500

    Authentication script auth.js, missing app validation in app.post command

---
+++ auth.js
@@ -0,0 +1,23 @@
+var passport = require("passport"), LocalStrategy = require("passport-local").Strategy;
+passport.use(new LocalStrategy({
+		usernameField: "email",
+		passwordField: "passwd",
+	},
+	function(username, password, done){
+		User.findOne({ username: username }, function(err,user){
+			if (err) {return done(err);}
+			if (!user){
+				return done(null, false, {message: "Incorrect username."});
+			}
+			if (!user.validPassword(password)){
+				return done(null,false, {message: "Incorrect password."});
+			}
+			return done(null,user);
+		});
+	}
+));
+app.post("/login",
+	passport.authenticate("local",{succesRedirect: "/",
+				       failureRedirect:"/login",
+                                       failureFlash: true})
+);
